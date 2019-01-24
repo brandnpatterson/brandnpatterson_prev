84aa2dcc60a3df4a10c122dd3171e82dfe224a3e
Brandon Patterson <brandnpatterson@gmail.com>
2019-01-23 23:03:55 -0600

    remove unuses

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,6 @@ const router = express.Router();
 const index = require('../controllers/indexController');
 const riot = require('../controllers/riotController');
 
-const errors = fn => {
-  console.log(fn);
-
-  return fn;
-};
-
 router.get('/riot/summoner/:summonerName', errors(riot.getSummonerInfo));
 router.get('/riot/summoner/:summonerName/mastery', riot.getChampionMastery);
 router.get('*', index);
