e640dad3ed72ca0507abf23f9ced1f4bc7296ddc
Brandon Patterson <brandnpatterson@gmail.com>
2019-01-27 21:13:16 -0600

    config

--- config/config.js
+++ config/config.js
@@ -11,11 +11,15 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME_TEST,
     host: process.env.DB_HOST,
     dialect: 'mysql'
   },
   production: {
-    use_env_variable: 'DATABASE_URL'
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
   }
 };
