6826dc1294eefa0a11db0992f9baf94b5ad1a48a
Brandon Patterson <brandnpatterson@gmail.com>
2019-03-13 15:34:27 -0500

    target blank

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <a href="BrandonPattersonResume.pdf">Here is a copy of my resume</a>
+      <a
+        href="BrandonPattersonResume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Here is a copy of my resume
+      </a>
     </div>
   );
 };
