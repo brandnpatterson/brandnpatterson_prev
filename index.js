c8b24aaa066a0333b2acc2a26612a283c2398fef
Brandon Patterson <brandnpatterson@gmail.com>
2019-04-05 11:55:22 -0500

    use compression

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./routes');
+const compression = require('compression');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'client/public')));
 app.use('/api/v1', routes);
+app.use(compression());
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/public/index.html'), err => {
