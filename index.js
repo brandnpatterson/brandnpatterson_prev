05fd627ce899bfbbbd3169637d619c3149840caf
Brandon Patterson <brandnpatterson@gmail.com>
2019-04-29 12:37:19 -0500

    revert

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/public')));
 app.use('/api/v1', routes);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'), err => {
+  res.sendFile(path.join(__dirname, 'client/public/index.html'), err => {
     if (err) {
       res.status(500).send(err);
     }
